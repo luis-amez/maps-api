84fdfef247ca50c5c40f3a6bfc3125948c2461c2
Luis Amez <luis.amez.vitoria@gmail.com>
2018-04-02 18:04:27 +0200

    Allow user to select destination

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function initMap() {
   // Display map
-  var center = {lat: 50.8429, lng: -0.1313};
+  var origin = {lat: 50.8429, lng: -0.1313};
+  var destination = null;
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 6,
-    center: center
+    center: origin
   });
 
   // Init directions
@@ -16,22 +17,33 @@ function initMap() {
   directionsDisplay.setMap(map);
 
   // Show route for default coordinates
-  calculateAndDisplayRoute(directionsService, directionsDisplay, center);
+  calculateAndDisplayRoute(directionsService, directionsDisplay, origin, destination);
 
-  // Add event listener for user selection
+  // Add event listener for user travel mode selection
   document.getElementById('mode').addEventListener('change', function() {
-    calculateAndDisplayRoute(directionsService, directionsDisplay, center);
+    calculateAndDisplayRoute(directionsService, directionsDisplay, origin, destination);
+  });
+
+  // Create the search box and link it to the UI element
+  var input = document.getElementById('destination-input');
+  var autocomplete = new google.maps.places.Autocomplete(input);
+
+  // Add event listener for user destination selection
+  autocomplete.addListener('place_changed', function() {
+    var place = autocomplete.getPlace();
+    destination = place.geometry.location;
+    calculateAndDisplayRoute(directionsService, directionsDisplay, origin, destination);
   });
 
   // Try HTML5 geolocation.
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      center = {
+      origin = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
-      map.setCenter(center);
-      calculateAndDisplayRoute(directionsService, directionsDisplay, center);
+      map.setCenter(origin);
+      calculateAndDisplayRoute(directionsService, directionsDisplay, origin, destination);
     }, function(error) {
       // Browser doesn't support geolocation
       alert("I don't know where you are, sorry. Let's say that you are in Brighton.");
@@ -40,14 +52,19 @@ function initMap() {
   }
 }
 
-function calculateAndDisplayRoute(directionsService, directionsDisplay, origin) {
+function calculateAndDisplayRoute(directionsService, directionsDisplay, origin, destination) {
   // Take user selection
   var selectedMode = document.getElementById('mode').value;
+
+  // Set Oxford as default destination
+  if (destination === null) {
+    destination = {lat: 51.7519, lng: -1.2578};
+  }
 
   // Display route to Oxford
   directionsService.route({
     origin: origin,
-    destination: {lat: 51.7519, lng: -1.2578},
+    destination: destination,
     travelMode: google.maps.TravelMode[selectedMode]
   }, function(response, status) {
     if (status == 'OK') {
